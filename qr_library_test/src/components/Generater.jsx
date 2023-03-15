4741b4b4a5585ef8fa19e6e380969de8ed57bf35
KHashimoto3 <85172807+KHashimoto3@users.noreply.github.com>
2023-03-15 17:13:15 +0900

    任意のQRコードを生成できるようにした。

--- qr_library_test/src/components/Generater.jsx
+++ qr_library_test/src/components/Generater.jsx
@@ -1,5 +1,23 @@
+import { useState } from "react";
+
+import {QRCodeSVG} from "qrcode.react";
+
 export const Generater = () => {
+
+    const [text, setText] = useState("");
+    const [generateText, setGenerateText] = useState("");
+
+    const generateQR = () => {
+        setGenerateText(text);
+    }
+
     return(
-        <h1>生成</h1>
+        <div>
+            <h1>QR生成</h1>
+            <p>内容を入力</p>
+            <input value={text} onChange={(event) => setText(event.target.value)} />
+            <button onClick={generateQR}>生成！</button><br></br>
+            <QRCodeSVG value={generateText} />
+        </div>
     );
 };
